39a080cd2bb342ad3fffc73ca361d72b0fa8e1fe
JunkJumper <jose.srifi@gmail.com>
2019-10-08 10:29:58 +0200

    create database.php

---
+++ php/database.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  ----------------------------------------------------------------------------
+ * |                                database.php                                |
+ * |                    Objectif : paramétrer la base de données                |
+ * |                                                                            |
+ * | @category   CategoryName                                                   |
+ * | @package    PackageName                                                    |
+ * | @author     JunkJumper                                                     |
+ * | @copyright  2019 - JunkJumper                                              |
+ * | @license    https://creativecommons.org/licenses/by/4.0/  License CC BY 4.0|
+ * | @since      File available since 10/08/2019                                |
+ *  ----------------------------------------------------------------------------
+ */
+
+/**
+ * Le but de ce script est de définir les variables pour vos connexions à vos BDD
+ */
+
+$servername = "localhost"; //Vous devez remplacer ici le "localhost" par l'adresse de votrebase de données
+$username = "root"; //Vous devez indiquer votre nom d'utilisateur pour accèder à la base de données
+$password = "root"; //Vous devez indiquer ici le mot de passe
+$dbname = "database"; //Vous devez remplace le "database" par le nom de la table de votre base de données.
+
+
+
+?>
